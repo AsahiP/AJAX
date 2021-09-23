8efc600a77c8494e2071f9258bd27261edbc6170
b <asahisemail@gmail.com>
2021-09-23 00:00:26 -0700

    Created new further-study.js, implemented code refactoring and .serialize()

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -17,11 +17,6 @@ $('#get-fortune-button').on('click', showFortune);
 
 // PART 2: SHOW WEATHER
 
-// $( "form" ).on( "submit", function( event ) {
-//   event.preventDefault();
-//   console.log( $( this ).serialize() );
-// });
-
 function showWeather(evt) {
   evt.preventDefault();
 
@@ -36,12 +31,10 @@ function showWeather(evt) {
     alert(`${res.temp}`);
     alert(`${res.forecast}`);
     
-  });
+//   });
 
-  // TODO: request weather with that URL and show the forecast in #weather-info
-}
-
-$('#weather-form').on('submit', showWeather);
+//   // TODO: request weather with that URL and show the forecast in #weather-info
+// }
 
 // PART 3: ORDER MELONS
 
@@ -49,9 +42,6 @@ $('#weather-form').on('submit', showWeather);
 
 function orderMelons(evt) {
   evt.preventDefault();
-
-  // const melonType = {melon_type: $("#melon-type-field" ).val()};
-  // const qty = {qty: $("#qty-field").val()};
 
   const formData = { 
     melon_type: $("#melon-type-field" ).val(),
@@ -67,22 +57,3 @@ function orderMelons(evt) {
 }
 
 $('#order-form').on('submit', orderMelons);
-
-// .serialize()
-// $( "input, textarea, select" ).serialize();
-
-// $( "form" ).on( "submit", function( event ) {
-//   event.preventDefault();
-//   console.log( $( this ).serialize() );
-// });
-// encode a set of form elements as a string for submission
-
-
-// .load( url [, data ] [, complete ] )
-// $( "#result" ).load( "ajax/test.html" );
-// Load data from the server and place the returned HTML into the matched elements.
-// is roughly equivalent to $.get(url, data, success)
-
-// $( "#result" ).load( "ajax/test.html #container" )
-
-// allows us to specify (in the url parameter) a portion of the remote document to be inserted 
